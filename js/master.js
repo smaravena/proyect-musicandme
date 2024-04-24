786e714ed51f966d6347941824bc23e960f2b1ac
Smolenks Aravena <aravena1440@gmail.com>
2024-04-24 12:54:40 -0400

    Add iframeResizer + Navbar.html

---
+++ js/master.js
@@ -0,0 +1,18 @@
+//iframeResizer
+iFrameResize({ log: false }, "#my_navbar");
+
+//TODO validaciones formulario carro
+function validaciones() {
+  //CODIGO
+}
+
+//TODO implementar api
+function api_insturmento() {
+  //CODIGO
+}
+
+//TODO agregar algo extra
+function cosa() {
+  //CODIGO
+  alert("test alert");
+}
