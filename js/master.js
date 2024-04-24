1e3d58e8c1589824f9f7c7877fe20b785a2860ff
Smolenks Aravena <aravena1440@gmail.com>
2024-04-24 18:23:12 -0400

    [Fix] footer as iframe, js class and width

--- js/master.js
+++ js/master.js
@@ -1,6 +1,9 @@
 //iframeResizer
 iFrameResize({ log: false }, "#my_navbar");
 
+//iframeResizer
+iFrameResize({ log: false }, "#masterFooter");
+
 //TODO validaciones formulario carro
 function validaciones() {
   //CODIGO
