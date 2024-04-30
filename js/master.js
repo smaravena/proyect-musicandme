bc5b0c8a88f137ac9982949ca9e1024663b21580
smaravena <aravena1440@gmail.com>
2024-04-30 10:07:51 -0400

    [JS] preparar validaciones en js

--- js/master.js
+++ js/master.js
@@ -19,3 +19,35 @@ function cosa() {
   //CODIGO
   alert("test alert");
 }
+
+function validarPass(){
+  var password=$('#idpass').val();
+
+  if (password.length==0){
+    return false;
+
+  } else {
+    return true;
+  }
+}
+
+
+function inicosesion(event){
+  event.preventDefault();
+  var messageError = document.getElementById('diverrorid');
+  var valido = true;
+  var errores='';
+
+  if (!validarPass()){
+    errores = errores + ' algo invalido\n';
+    valido=false;
+
+    
+  }
+  if(!valido){
+    messageError.style.display='block';
+    messageError.innerHTML=errores;
+  }
+  //HAY QUE METER CLASE EN DIV DE BLOQUE DE ERROR
+  //HAY QUE METER ID EN CAMPO DE FORM
+}
