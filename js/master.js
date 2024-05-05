ab2fcb37e47aa415b36f7171a2f9bcd5b90445a7
Smolenks Aravena <aravena1440@gmail.com>
2024-05-05 17:35:30 -0400

    [API] Implementacion generica

--- js/master.js
+++ js/master.js
@@ -51,25 +51,3 @@ function inicosesion(event){
   //HAY QUE METER CLASE EN DIV DE BLOQUE DE ERROR
   //HAY QUE METER ID EN CAMPO DE FORM
 }
-
-const APIController = (function() {
-    
-  const clientId = 'ee9ebbd6aac24ccca4d935b3845fb6d7';
-  const clientSecret = 'eac099fbca0a4b9b870c7105a3cf6d7b';
-
-  // private methods
-  const _getToken = async () => {
-
-      const result = await fetch('https://accounts.spotify.com/api/token', {
-          method: 'POST',
-          headers: {
-              'Content-Type' : 'application/x-www-form-urlencoded', 
-              'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
-          },
-          body: 'grant_type=client_credentials'
-      });
-
-      const data = await result.json();
-      return data.access_token;
-  }
-});
