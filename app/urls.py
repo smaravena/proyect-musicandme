6f55b03ba5f9220043a1cc9510eb22eac0f9c121
Smolenks Aravena <aravena1440@gmail.com>
2024-06-03 19:01:14 -0400

    import more htmls

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, discos
+from .views import home, discos, instrumentos, carro, login
 
 urlpatterns = [
     path('index', home, name="home"),
     path('discos', discos, name="discos"),
+    path('instrumentos', instrumentos, name="instrumentos"),
+    path('carro', carro, name="carro"),
+    path('login', login, name="login"),
 ]
