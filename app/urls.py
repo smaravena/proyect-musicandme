d22aee100867679926262c2774d04861fbe639f6
Smolenks Aravena <aravena1440@gmail.com>
2024-06-03 18:37:47 -0400

    django urls, views, html's, etc

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, discos
 
 urlpatterns = [
-    path('', home, name="home"),
+    path('index', home, name="home"),
+    path('discos', discos, name="discos"),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'app/home.html')
+
+def discos(request):
+    return render(request, 'app/discos.html')
