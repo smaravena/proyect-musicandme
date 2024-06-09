515d27cc47813f7d6afcddf5ebe3de6609cc59df
Smolenks Aravena <aravena1440@gmail.com>
2024-06-09 18:37:15 -0400

    preparar bd para mostrar

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Genero, Disco, Tipo_instrumento, Instrumento
 
 # Create your views here.
 
 def home(request):
+    contex = {}
     return render(request, 'app/home.html')
 
 def discos(request):
@@ -16,3 +18,11 @@ def carro(request):
 
 def login(request):
     return render(request, 'app/login.html')
+
+# VISTA BASE DE DATOS
+#def discos_list(request):
+    ##DEFINIR OBJ PARA TRAER DE LA BD
+    #Alumno.objects.all() <=> 'Select * From Alumnos'
+    discos = Disco.objects.all() 
+    contex = {'discos' : discos}
+    return render(request,'app/discos_list.html',contex)
