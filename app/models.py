bfeb3cd99a00dd933b5a95b6a1dca48f991e95a7
Smolenks Aravena <aravena1440@gmail.com>
2024-06-10 09:12:32 -0400

    add spacing

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+# Create your models here.
 class Genero(models.Model):
     id_genero  = models.AutoField(db_column='idGenero', primary_key=True) 
     genero     = models.CharField(max_length=20, blank=False, null=False)
@@ -17,12 +19,14 @@ class Disco(models.Model):
 
     def __str__(self):
         return str(self.nombre)+"-"+str(self.artista)
+    
 class Tipo_instrumento(models.Model):
     id_tipo  = models.AutoField(db_column='id_tipo', primary_key=True) 
     tipo     = models.CharField(max_length=20, blank=False, null=False)
 
     def __str__(self):
         return str(self.tipo)
+    
 class Instrumento(models.Model):
     id_instrumento   = models.IntegerField(primary_key=True)
     marca            = models.CharField(max_length=20,null=False)
@@ -31,7 +35,6 @@ class Instrumento(models.Model):
     id_tipo          = models.ForeignKey('Tipo_instrumento',on_delete=models.CASCADE, db_column='id_tipo')  
     imagen           = models.ImageField(upload_to="instrumento", null=True)
     descripcion      = models.CharField(max_length=200,null=False)
+    
     def __str__(self):
         return str(self.nombre)+"-"+str(self.marca)
-          
-# Create your models here.
