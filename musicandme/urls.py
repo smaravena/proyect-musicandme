c6f95eb7d81dcb6e34c07e79d174e813c1956789
Matias <mr.gonzalez@duocuc.cl>
2024-06-11 10:36:06 -0400

    app admin añadida + cambios img discos

--- musicandme/urls.py
+++ musicandme/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuario/', include('app.urls')),
+    path('admin/', include('admin.urls')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
