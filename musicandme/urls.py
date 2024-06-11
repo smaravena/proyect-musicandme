5ab252495553672b328f8a97e95e16a4858a9975
Matias Gonzalez <gonzalezpizarrom98@gmail.com>
2024-06-11 17:26:16 -0400

    app admin

--- musicandme/urls.py
+++ musicandme/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuario/', include('app.urls')),
+    path('administrador/',include('administrador.urls'))
 ]
 
 if settings.DEBUG:
