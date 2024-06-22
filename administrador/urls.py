9c239d50ff7c72290b2b6fb97cf720c474bc479a
Matias Gonzalez <gonzalezpizarrom98@gmail.com>
2024-06-22 17:39:21 -0400

    listadiscosadmin+importBD

--- administrador/urls.py
+++ administrador/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homeadm
+from .views import homeadm,viewDisco
 
 urlpatterns = [
     path('homeadm', homeadm, name="homeadm"),
+    path('viewdisco', viewDisco, name="viewdisco"),
 ]
--- administrador/views.py
+++ administrador/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from app.models import Genero,Disco,Tipo_instrumento,Instrumento
 # Create your views here.
 @login_required
 def homeadm(request):
     context = {}
     return render(request,'administrador/test.html',context)
+@login_required
+def viewDisco(request):
+    discos=Disco.objects.all()
+    return render(request, 'administrador/lista_discosAdm.html', {'discos': discos})
+
+
 
