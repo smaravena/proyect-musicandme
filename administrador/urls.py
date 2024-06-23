b184d48e415179fef69bab3c7771c41105a8537d
Matias Gonzalez <gonzalezpizarrom98@gmail.com>
2024-06-23 17:25:37 -0400

    CRUDdisco-completo(diseñofeo)

--- administrador/urls.py
+++ administrador/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import index,viewDisco,viewInstrum,discoAdd
+from .views import index,viewDisco,viewInstrum,discoAdd,discoDelete,discoEdit
 
 urlpatterns = [
     path('index', index, name="index"),
     path('viewdisco', viewDisco, name="viewdisco"),
     path('viewinstrum', viewInstrum, name="viewinstrum"),
     path('discoadd',discoAdd, name="discoadd"),
+    path('discoedit/<str:pk>', discoEdit, name='discoedit'),
+    path('discodelete/<str:pk>', discoDelete, name='discodelete'),
 ]
