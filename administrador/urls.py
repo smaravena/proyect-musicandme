79a42b3e5024d2c0f17857f82d45db08cbf9e65d
chucu <mr.gonzalez@duocuc.cl>
2024-06-24 09:34:04 -0400

    Revert "CRUDdisco-completo(diseñofeo)"
    This reverts commit b184d48e415179fef69bab3c7771c41105a8537d.

--- administrador/urls.py
+++ administrador/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import index,viewDisco,viewInstrum,discoAdd,discoDelete,discoEdit
+from .views import index,viewDisco,viewInstrum,discoAdd
 
 urlpatterns = [
     path('index', index, name="index"),
     path('viewdisco', viewDisco, name="viewdisco"),
     path('viewinstrum', viewInstrum, name="viewinstrum"),
     path('discoadd',discoAdd, name="discoadd"),
-    path('discoedit/<str:pk>', discoEdit, name='discoedit'),
-    path('discodelete/<str:pk>', discoDelete, name='discodelete'),
 ]
