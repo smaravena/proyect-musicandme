cade53364b7a6f6d7d2f77b529ebece1e26ece8b
Smolenks Aravena <aravena1440@gmail.com>
2024-07-12 17:42:50 -0400

    CARRITO BD

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Genero, Disco, Tipo_instrumento, Instrumento
+from .models import Genero, Disco, Tipo_instrumento, Instrumento, Carrito, ElementoCarrito
 
 # Register your models here.
 admin.site.register(Genero)
 admin.site.register(Disco)
 admin.site.register(Tipo_instrumento)
 admin.site.register(Instrumento)
+admin.site.register(Carrito)
+admin.site.register(ElementoCarrito)
